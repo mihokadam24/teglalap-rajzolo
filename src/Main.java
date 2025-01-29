09e5514815e35a5a0ee7dee6a22f23a2ef065b8f
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 22:44:28 +0100

    Implemented spaghetti code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // kérjen be a felhasználótól adatokat (a téglalap szélessége és magassága), a megjelenítendő karakter, és razoljon ki egy ilyen méretű téglalapot a megadott karakterből
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kérem a téglalap szélességét: ");
+        int szelesseg = scanner.nextInt();
+        System.out.print("Kérem a téglalap magasságát: ");
+        int magassag = scanner.nextInt();
+        System.out.print("Kérem a rajzoló karaktert: ");
+        String character = scanner.next();
+        // kirajzolás
+        for (int i = 0; i < magassag; i++) {
+            for (int j = 0; j < szelesseg; j++) {
+                System.out.print(character + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
