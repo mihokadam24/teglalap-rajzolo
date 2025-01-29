7e197a9df1e061b79a30c4211385d8e67d8c9840
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 22:39:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // kérjen be a felhasználótól adatokat (a téglalap szélessége és magassága), a megjelenítendő karakter, és razoljon ki egy ilyen méretű téglalapot a megadott karakterből
+
+    }
+}
